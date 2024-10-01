caa0ffe6756843e98550211d45d4e43ee10663ab
Al3xiel <gabriel280104casas@gmail.com>
2024-10-01 17:55:15 -0500

    feat(language-switcher): Added switcher component.

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import './style.css'
 import App from './App.vue'
 import i18n from "./i18n.js";
 import {router} from "json-server";
+import SelectButton from "primevue/selectbutton";
 
 const app = createApp(App);
+
+app.component('pv-select-button', SelectButton)
 
 app.use(i18n);
 
