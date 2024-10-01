b3d60489e5ed1719e816e179034cadaac2f57b9f
Al3xiel <gabriel280104casas@gmail.com>
2024-10-01 14:24:25 -0500

    feat(routes): setup routes support.

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import i18n from "./i18n.js";
+import {router} from "json-server";
 
 const app = createApp(App);
 
 app.use(i18n);
+
+app.use(router);
 
 app.mount('#app');
