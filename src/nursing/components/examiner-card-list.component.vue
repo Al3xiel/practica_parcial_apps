c1f7919fd361124a6a82735a4b6cd28f51086a41
Al3xiel <gabriel280104casas@gmail.com>
2024-10-01 22:49:01 -0500

    feat(examiner-performance-overview): added examiner card list component.

--- src/nursing/components/examiner-card-list.component.vue
+++ src/nursing/components/examiner-card-list.component.vue
@@ -1,13 +1,76 @@
 <script>
+import ExaminerCard from "./examiner-card.component.vue";
+import {Examiner} from "../model/examiner.entity.js";
+import {ExaminerService} from "../services/examiner.service.js";
+import {MentalStateExam} from "../../shared/model/mental-state-exam.entity.js";
+import {MentalStateExamService} from "../../shared/services/mental-state-exam.service.js";
+
 export default {
-  name: "examiner-card-list"
+  name: "examiner-card-list",
+  components: {ExaminerCard},
+  data(){
+    return{
+      examiners: [],
+      exams: [],
+      examsCount: [],
+      averagesTotalScores: [],
+      examiner: new Examiner({}),
+      exam: new MentalStateExam({}),
+      examinersService: null,
+      mentalStateExamService: null
+    }
+  },
+  methods:{
+    getExamsCount() {
+      this.examsCount = [];
+      this.examiners.forEach((examiner, index) => {
+        this.examsCount[index] = this.exams.filter(exam => exam.examinerId === examiner.id).length;
+      });
+    },
+    getAveragesTotalScores() {
+      this.averagesTotalScores = [];
+      this.examiners.forEach((examiner, index) => {
+        let totalScores = this.exams.filter(exam => exam.examinerId === examiner.id).map(exam => exam.orientationScore + exam.registrationScore + exam.attentionAndCalculationScore + exam.recallScore + exam.languageScore);
+        this.averagesTotalScores[index] = totalScores.reduce((a, b) => a + b, 0) / totalScores.length;
+      });
+    }
+  },
+  created() {
+    this.examinersService = new ExaminerService();
+    this.mentalStateExamService = new MentalStateExamService();
+
+    this.examinersService.getAll().then(response => {
+      this.examiners = response.data.map(examiner => new Examiner(examiner));
+      console.log(this.examiners);
+      return this.mentalStateExamService.getAll();
+    }).then(response => {
+      this.exams = response.data.map(exam => new MentalStateExam(exam));
+      console.log(this.exams);
+      this.getExamsCount();
+      this.getAveragesTotalScores();
+      console.log(this.examsCount);
+      console.log(this.averagesTotalScores);
+    }).catch(error => console.error(error));
+  }
 }
 </script>
 
 <template>
-
+  <div class="card-container" aria-label="examiner list">
+    <examiner-card
+        v-for="(examiner, index) in examiners"
+        :key="examiner.id"
+        :examiner="examiner"
+        :examCount="examsCount[index]"
+        :averageExamTotal="averagesTotalScores[index]"
+    />
+  </div>
 </template>
 
 <style scoped>
-
+.card-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+}
 </style>
