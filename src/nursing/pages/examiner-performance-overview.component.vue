d6baa27298fe8355f9659c50136eaeb016f5140d
Al3xiel <gabriel280104casas@gmail.com>
2024-10-01 22:49:20 -0500

    feat(examiner-performance-overview): implemented examiner card list component.

--- src/nursing/pages/examiner-performance-overview.component.vue
+++ src/nursing/pages/examiner-performance-overview.component.vue
@@ -1,11 +1,15 @@
 <script>
+import ExaminerCardList from "../components/examiner-card-list.component.vue";
+
 export default {
-  name: "examiner-performance-overview"
+  name: "examiner-performance-overview",
+  components: {ExaminerCardList}
 }
 </script>
 
 <template>
   <h1>“Examiner Performance Overview</h1>
+  <examiner-card-list/>
 </template>
 
 <style scoped>
