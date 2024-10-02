8a65b90a2de44ef86cc9cab4a17e4470e322a6e5
Al3xiel <gabriel280104casas@gmail.com>
2024-10-01 19:13:22 -0500

    feat(toolbar): Added toolbar-header component.

--- src/main.js
+++ src/main.js
@@ -2,12 +2,16 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import i18n from "./i18n.js";
-import {router} from "json-server";
+import router from "./router/index.js";
 import SelectButton from "primevue/selectbutton";
+import Toolbar from "primevue/toolbar";
+import Button from "primevue/button";
 
 const app = createApp(App);
 
 app.component('pv-select-button', SelectButton)
+    .component('pv-toolbar', Toolbar)
+    .component('pv-button', Button)
 
 app.use(i18n);
 
---
+++ src/public/components/toolbar-header.component.vue
@@ -0,0 +1,60 @@
+<script>
+
+import LanguageSwitcher from "./language-switcher.component.vue";
+
+export default {
+  name: "toolbar-header",
+  components: {LanguageSwitcher},
+  data(){
+    return {
+      drawer: false,
+      items: [
+        {label: "Home", to: '/home'},
+        {label: "Examiner Performance Overview", to: '/nursing/examiner-performance-overview'}
+      ]
+    }
+  },
+  methods: {
+    toggleDrawer(){
+      this.drawer = !this.drawer;
+    }
+  }
+}
+</script>
+
+<template>
+  <header class = "header-toolbar">
+    <pv-toolbar style="padding: 5px; background-color: #D1E4FA; border-style: none; color: black;">
+      <template #start>
+        <img src="https://hign.org/sites/default/files/2022-01/HIGN-logo-1200.png" alt="HIGN-logo" class="toolbar-img">
+        <p style="font-weight: bold; padding-left: 10px;">Hartford Institute for Geriatric Nursing</p>
+      </template>
+      <template #end>
+        <div>
+          <router-link v-for="item in items" :keys="item.label" v-slot="{navigate, href}" :to="item.to" custom>
+            <pv-button :href="href" class="custom-button" @click="navigate" text plain><p style="margin:0;">{{item.label}}</p></pv-button>
+          </router-link>
+        </div>
+        <language-switcher/>
+      </template>
+    </pv-toolbar>
+  </header>
+</template>
+
+<style scoped>
+  .toolbar-img{
+    height: 30px;
+  }
+  .header-toolbar {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    z-index: 1000;
+    height: fit-content;
+  }
+
+  .custom-button {
+    margin: 20px;
+  }
+</style>
