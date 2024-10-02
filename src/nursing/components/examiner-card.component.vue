666863e0288f93c4f54c2ddb1e5b9141166041d8
Al3xiel <gabriel280104casas@gmail.com>
2024-10-01 22:48:46 -0500

    feat(examiner-performance-overview): added examiner card component.

--- src/nursing/components/examiner-card.component.vue
+++ src/nursing/components/examiner-card.component.vue
@@ -1,11 +1,34 @@
 <script>
+import {Examiner} from "../model/examiner.entity.js";
+
 export default {
-  name: "examiner-card"
+  name: "examiner-card",
+  props:{
+    examiner:{type: Examiner, required: true},
+    examCount: {type: Number, required: true},
+    averageExamTotal: {type: Number, required: true}
+  }
 }
 </script>
 
 <template>
-
+  <pv-card>
+    <template #header>
+      {{examiner.firstName}} {{examiner.lastName}}
+    </template>
+    <template #title>
+      {{$t('examinerCard.cardTitle')}}{{examiner.nationalProviderIdentifier}}
+    </template>
+    <template #subtitle>
+      {{$t('examinerCard.cardSubtitle')}}
+    </template>
+    <template #content>
+      <ul>
+        <li>{{$t('examinerCard.examAssignedCount')}} {{examCount}}</li>
+        <li>{{$t('examinerCard.averageAssignedExamTotal')}} {{averageExamTotal}}</li>
+      </ul>
+    </template>
+  </pv-card>
 </template>
 
 <style scoped>
