f34c576b5e287fa4753ecbc2e36a33ef27452ee8
Al3xiel <gabriel280104casas@gmail.com>
2024-10-01 19:15:50 -0500

    feat(routes): Enabled routing.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,14 @@
 import {createRouter, createWebHistory} from "vue-router";
+import HomeComponent from "../public/pages/home.component.vue";
+import ExaminerPerformanceOverviewComponent from "../nursing/pages/examiner-performance-overview.component.vue";
+import NotFoundComponent from "../public/pages/not-found.component.vue";
 const router = createRouter({
     history: createWebHistory(),
-    routes: []
+    routes: [
+        {path: '/home', name:'Home', component: HomeComponent},
+        {path:'/nursing/examiner-performance-overview', name:'Examiner Performance Overview' ,component:ExaminerPerformanceOverviewComponent},
+        {path: '/', redirect: '/home'},
+        {path: '/:pathMatch(.*)*', name: 'not-found', component: NotFoundComponent}
+    ]
 });
 export default router;
